935fc7069dc61ca02b913851c483ab6ed1c886d6
Poli kanaka uday kumar reddy <poliudhaykumar@gmail.com>
2026-08-21 16:05:39 +0530

    Refactor knapsack algorithm to improve efficiency
    Refactored knapsack implementation to use dynamic programming with a 1D array and track selected items.

--- Practical-5/KnapSack_By_Dynamic_Programming.cpp
+++ Practical-5/KnapSack_By_Dynamic_Programming.cpp
@@ -2,35 +2,51 @@
 #include <vector>
 using namespace std;
 
-int main() {
-    int wt[] = {1, 2, 5, 6};
-    int val[] = {2, 3, 4, 5};
+int knapsack(int W, vector<int> wt, vector<int> val) {
 
-    int n = 4;
-    int W = 8;
+    int n = val.size();
 
-    int dp[n + 1][W + 1];
+    vector<int> dp(W + 1, 0);
+    vector<vector<bool>> selected(n, vector<bool>(W + 1, false));
 
-    // Initialize DP table
-    for (int i = 0; i <= n; i++) {
-        for (int w = 0; w <= W; w++) {
+    for (int i = 0; i < n; i++) {
 
-            if (i == 0 || w == 0) {
-                dp[i][w] = 0;
-            }
-            else if (wt[i - 1] <= w) {
-                dp[i][w] = max(
-                    dp[i - 1][w],
-                    val[i - 1] + dp[i - 1][w - wt[i - 1]]
-                );
-            }
-            else {
-                dp[i][w] = dp[i - 1][w];
+        for (int w = W; w >= wt[i]; w--) {
+
+            if (dp[w] < dp[w - wt[i]] + val[i]) {
+                dp[w] = dp[w - wt[i]] + val[i];
+                selected[i][w] = true;
             }
         }
     }
 
-    cout << "Maximum value = " << dp[n][W] << endl;
+    // Find selected items
+    int w = W;
+
+    cout << "Selected items:" << endl;
+
+    for (int i = n - 1; i >= 0; i--) {
+        if (selected[i][w]) {
+            cout << "Weight = " << wt[i]
+                 << ", Profit = " << val[i] << endl;
+
+            w = w - wt[i];
+        }
+    }
+
+    return dp[W];
+}
+
+int main() {
+
+    int capacity = 8;
+
+    vector<int> profits = {1, 2, 5, 6};
+    vector<int> weights = {2, 3, 4, 5};
+
+    int result = knapsack(capacity, weights, profits);
+
+    cout << "Maximum profit: " << result << endl;
 
     return 0;
 }
